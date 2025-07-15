eb43540ef128c28360697beddae8ced3af13d29b
Zelticu <Zelticue@gmail.com>
2025-07-14 22:30:24 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd /app
-./xmrig/xmrig -o wownero.herominers.com:1123 \
--u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
---coin wownero -p x --threads 1 --cpu-priority 0 --donate-level 0
+
+# Run mining with 1 thread, lowest CPU, no donation
+./xmrig/xmrig \
+  -o pool.wowne.ro:3333 \
+  -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
+  -p x \
+  --coin wownero \
+  --threads 1 \
+  --cpu-priority 0 \
+  --donate-level 0
