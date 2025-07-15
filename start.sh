68e1f12b4abd0b7a2d3554bf712d09fd756775ad
Zelticu <Zelticue@gmail.com>
2025-07-14 22:39:17 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 cd /app
 
-# Run mining with 1 thread, lowest CPU, no donation
 ./xmrig/xmrig \
-  -o pool.wowne.ro:3333 \
+  -o stratum+tcp://pool.wowne.ro:3333 \
   -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
   -p x \
   --coin wownero \
