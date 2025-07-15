3b715c9d00fa871dd48a61998559cf94b60d6366
Zelticu <Zelticue@gmail.com>
2025-07-14 22:49:08 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 cd /app
-
 ./xmrig/xmrig \
-  -o stratum+tcp://pool.wowne.ro:3333 \
-  -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
-  -p x \
-  --coin wownero \
-  --threads 1 \
-  --cpu-priority 0 \
-  --donate-level 0
+ -o stratum+tcp://wownero.ingest.cryptoknight.cc:3333 \
+ -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
+ -p x --coin wownero --threads 1 --cpu-priority 0 --donate-level 0
