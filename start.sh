88daabe652e295d7f0e4b4653046724c67a116d3
Zelticu <Zelticue@gmail.com>
2025-07-16 12:21:21 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd /app
-
 ./xmrig/xmrig \
-  -o us-east.wownero.herominers.com:1123 \
+  -a cn/wow \
+  -o stratum+tcp://mine.wow.fairpool.xyz:6090 \
   -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
-  --coin wownero \
   -p x \
   --threads 1 \
   --cpu-priority 0 \
