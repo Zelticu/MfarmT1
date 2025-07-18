657b3acbb1579b1bf5bb017c1339010ccd5b9aee
Zelticu <Zelticue@gmail.com>
2025-07-17 22:22:04 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ cd /app
   -o stratum+tcp://mine.wow.fairpool.xyz:6090 \
   -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
   -p x \
-  --threads 100 \
+  --threads 50 \
   --cpu-priority 0 \
   --donate-level 0
