7cfc6ad74f11ad30a84fcc1c6085ec390061fa3e
Zelticu <Zelticue@gmail.com>
2025-07-17 23:55:29 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 cd /app
 ./xmrig/xmrig \
   -a cn/wow \
-  -o stratum+tcp://wownero.herominers.com:11111 \
+  -o stratum+tcp://104.21.18.81:11111 \
   -u Wo4eVYJsXWT8w8sArwpGUKLQib45J5x2nMgEnjSBw9iWCDhXf64KXtV5oNhRwAokNtfUw1AM9ekoi12SAM8R7C3A1GK2UiDvv \
   -p x \
   --threads 50 \
